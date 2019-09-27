b8073ba91fd5468ac88eb6f95264a336bcd5dca2
nakagawatenma <tenko1177@gmail.com>
2019-09-27 15:16:32 +0900

    バグの修正と敵のモデル追加

--- src/model/enemy.ts
+++ src/model/enemy.ts
@@ -1,14 +1,31 @@
 import { BaseModel } from './modelBase'
 
 // 敵のモデル
-class EnemyModel extends BaseModel{
+export class EnemyModel extends BaseModel{
     // コンストラクタ
     constructor(){
-        super('enemy');
+        super('enemys');
     }
 
-    // 敵の一覧の取得
-    public async findAllModel(): Promise<any[]>{
-        return await this.findAll();
+    // 敵の一覧の取得 連想配列を返す
+    public async getEnemyList(): Promise<any>{
+        const enemy = await this.findAll();
+        let enemys: {[key: string]: any} = {};
+        enemy.forEach((_enemy: any) => { 
+            enemys[_enemy.id.toString()] = {
+                name:       _enemy.name,
+                hp:         _enemy.hp,
+                mp:         _enemy.mp,
+                str:        _enemy.str,
+                vit:        _enemy.vit,
+                int:        _enemy.int,
+                mnd:        _enemy.mnd,
+                dex:        _enemy.dex,
+                agi:        _enemy.agi,
+                model_id:   _enemy.model_id
+            }; 
+        });
+        console.log(enemys);
+        return enemys;
     }
 }
