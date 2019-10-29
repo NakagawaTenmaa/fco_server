74d0eee8a58288e5fbee58ae2e112ebeb9544f8b
nakagawatenma <tenko1177@gmail.com>
2019-10-29 20:21:43 +0900

    クラスごとの送信個所を止める

--- src/ishitaka/Player.ts
+++ src/ishitaka/Player.ts
@@ -514,7 +514,8 @@ export class Player implements Character{
         const z : number = this.transform.worldMatrix.column1.z;
         data.dir = Math.atan2(z, x);
 
-        return CharacterManager.instance.Send(this.id, this.mapId, JSON.stringify(data));
+        return true;
+        //return CharacterManager.instance.Send(this.id, this.mapId, JSON.stringify(data));
     }
 
     /**
@@ -532,6 +533,7 @@ export class Player implements Character{
         data.mp = 100.0 * this.status.magicPoint / this.status.maxMagicPoint;
         data.status = this.status.abnormalConditionStatus.flag;
 
-        return CharacterManager.instance.Send(this.id, this.mapId, JSON.stringify(data));
+        return true;
+        //return CharacterManager.instance.Send(this.id, this.mapId, JSON.stringify(data));
     }
 }
