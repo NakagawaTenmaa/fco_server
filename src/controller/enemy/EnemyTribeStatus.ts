1153637f7b0fc3c64d6aacb93b07109418354ac9
nakagawatenma <tenko1177@gmail.com>
2020-02-07 00:20:30 +0900

    エラーの修正

--- src/controller/enemy/EnemyTribeStatus.ts
+++ src/controller/enemy/EnemyTribeStatus.ts
@@ -135,6 +135,7 @@ export class EnemyTribeStatus implements Status{
     public constructor(){
         this.tribeData_ = new EnemyTribeData(
             0,
+            0,
             'None',
             0,
             0,
